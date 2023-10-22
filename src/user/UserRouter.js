87c12e8c4eb638bd627a42e3d87c0175fbb5a41a
ngel rojas <aomarrojasp@gmail.com>
2023-10-22 20:21:56 -0300

    add: adding validation the one field like a username

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,6 +3,14 @@ const UserService = require('./UserService');
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
+  const user = req.body;
+  if (user.username === null) {
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null, ok',
+      },
+    });
+  }
   await UserService.save(req.body);
   return res.status(201).send({ message: 'user created' });
 });
